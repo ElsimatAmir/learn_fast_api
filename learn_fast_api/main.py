bca7b9d43a2dc4ea36291c3ac694621c584c35af
ElcimatAmir <elcimatamir@gmail.com>
2023-04-22 08:37:25 +0300

    added a gitignore

--- learn_fast_api/main.py
+++ learn_fast_api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -20,3 +21,7 @@ async def shutdown():
 @app.get('/')
 async def root():
     return {"hello": "world"}
+
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
